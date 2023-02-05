f44a25489ac212d73f9a3f4f9b3fd7b5164cc35f
TuColegaDev <amdevelop@open-bootcamp.com>
2023-02-05 10:23:47 +0100

    feat: Testing Concepts

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { json } = require('body-parser');
+const cors = require('cors');
+const typeDefs = require('./modules/@apostrophecms/graphql/typedefs');
+const resolvers = require('./modules/@apostrophecms/graphql/resolvers');
+
 module.exports = {
-    bundle: {
-        directory: 'modules',
-        modules: [ '@apostrophecms/graphql' ]
-      },
+  async init(self) {
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers
+    });
+
+    await server.start();
+
+    self.apos.app.use('/api/v1/graphql', cors(), json(), expressMiddleware(server));
+  }
 };
