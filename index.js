09c4517da72b9cb0aa6f1807b1bdff5a6324f788
TuColegaDev <amdevelop@open-bootcamp.com>
2023-02-05 07:43:04 +0100

    feat: Concept Test

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Viva Rust
+// const { ApolloServer } = require('@apollo/server');
+// const { expressMiddleware } = require('@apollo/server/express4');
+// const { json } = require('body-parser');
+// const cors = require('cors');
+
+// module.exports = {
+//   async init(self) {
+//     const typeDefs = `#graphql
+
+//         type Query {
+//           books: [Book]!
+//         }
+
+//         type Book {
+//           name: String
+//         }
+//     `;
+
+//     const resolvers = {
+//       Query: {
+//         books: () => [{ name: 'HOLA SOY UN LIBRO' }]
+//       }
+//     };
+//     const server = new ApolloServer({
+//       typeDefs,
+//       resolvers
+//     });
+
+//     await server.start();
+
+//     self.apos.app.use('/api/v1/graphql', cors(), json(), expressMiddleware(server));
+//   }
+// };
+
+module.exports = {
+    init(self){
+        console.log("Estoy cargando el modulo de GraphQL... VAIS A FLIPAR")
+    }
+}
